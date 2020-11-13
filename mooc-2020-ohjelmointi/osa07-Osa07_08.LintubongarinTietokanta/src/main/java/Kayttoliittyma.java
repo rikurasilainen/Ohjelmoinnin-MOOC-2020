b67a121ddfed3120206d67fabda02759992fc2e4
rikurasilainen <36788504+rikurasilainen@users.noreply.github.com>
2020-11-13 10:14:31 +0200

    Update Kayttoliittyma.java

--- mooc-2020-ohjelmointi/osa07-Osa07_08.LintubongarinTietokanta/src/main/java/Kayttoliittyma.java
+++ mooc-2020-ohjelmointi/osa07-Osa07_08.LintubongarinTietokanta/src/main/java/Kayttoliittyma.java
@@ -1,15 +1,6 @@
 
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author Vapa MkIV
- */
 public class Kayttoliittyma {
 
     private Scanner lukija;
